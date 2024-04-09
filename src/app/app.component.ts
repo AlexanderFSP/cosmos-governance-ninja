cf6e7c555aea991a21be83639e3b9e6063c062e1
AlexanderFSP <alexanderfsp@mail.ru>
2024-04-09 13:13:22 -0300

    ✨ Implement temporary handler, which initiate transaction with 2 yes votes on proposals 897 & 899

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { WINDOW } from '@ng-web-apis/common';
+import { Window } from '@keplr-wallet/types';
+import { COSMOS_HUB_CHAIN_ID, COSMOS_HUB_RPC } from './constants/cosmos-hub';
+import {
+  GasPrice,
+  MsgVoteEncodeObject,
+  SigningStargateClient,
+} from '@cosmjs/stargate';
 
 @Component({
   standalone: true,
@@ -7,4 +15,59 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly window = inject<Window>(WINDOW);
+
+  /**
+   * Temporary handler, which initiate transaction with 2 yes votes on proposals 897 & 899
+   *
+   * TODO: (AlexanderFSP) Decouple logic into services
+   */
+  protected async onVote(): Promise<void> {
+    if (!this.window.keplr) {
+      throw new Error('Install Keplr');
+    }
+
+    await this.window.keplr.enable(COSMOS_HUB_CHAIN_ID);
+
+    /**
+     * Decided to use `getOfflineSignerOnlyAmino` instead of `getOfflineSigner`, because for Ledger I get such kind
+     * of error: "Error: SIGN_MODE_DIRECT can’t be signed on Ledger. Contact the web app provider to fix this issue."
+     *
+     * TODO: (AlexanderFSP) Understand the difference between `OfflineAminoSigner` and `OfflineDirectSigner`..
+     */
+    const offlineSigner =
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      this.window.getOfflineSignerOnlyAmino!(COSMOS_HUB_CHAIN_ID);
+
+    const signingClient = await SigningStargateClient.connectWithSigner(
+      COSMOS_HUB_RPC,
+      offlineSigner,
+      {
+        gasPrice: GasPrice.fromString('0.025ucosm'),
+      }
+    );
+
+    const voter = (await offlineSigner.getAccounts())[0].address;
+    const messages: MsgVoteEncodeObject[] = [
+      {
+        typeUrl: '/cosmos.gov.v1beta1.MsgVote',
+        value: {
+          proposalId: BigInt(897),
+          voter,
+          option: 1,
+        },
+      },
+      {
+        typeUrl: '/cosmos.gov.v1beta1.MsgVote',
+        value: {
+          proposalId: BigInt(899),
+          voter,
+          option: 1,
+        },
+      },
+    ];
+
+    await signingClient.signAndBroadcast(voter, messages, 'auto');
+  }
+}
---
+++ src/app/constants/cosmos-hub.ts
@@ -0,0 +1,2 @@
+export const COSMOS_HUB_CHAIN_ID = 'cosmoshub-4';
+export const COSMOS_HUB_RPC = 'https://cosmos-rpc.publicnode.com:443';
