b9092edec9cc0902e013467fb60b53f37afe2c83
AlexanderFSP <alexanderfsp@mail.ru>
2024-04-18 19:38:52 -0300

    🚧 Add status icons for proposals

--- src/app/components/proposal-list/components/proposal-card/proposal-card.component.ts
+++ src/app/components/proposal-list/components/proposal-card/proposal-card.component.ts
@@ -1,6 +1,20 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
-import { IProposal } from '../../../../models/paginated-proposals.model';
+import { IProposal, ProposalStatus } from '../../../../models/paginated-proposals.model';
+
+/**
+ * TODO: (AlexanderFSP) Add icons `PROPOSAL_STATUS_UNSPECIFIED` & `PROPOSAL_STATUS_DEPOSIT_PERIOD` (?)
+ *
+ * @see https://icons8.com/icons/authors/g26qDX9Ae6NS/icongeek26/external-icongeek26-linear-colour-icongeek26/external-politic-icongeek26-linear-colour-icongeek26
+ */
+const STATUS_ICON: Record<ProposalStatus, string | null> = {
+  [ProposalStatus.PROPOSAL_STATUS_UNSPECIFIED]: null,
+  [ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD]: null,
+  [ProposalStatus.PROPOSAL_STATUS_VOTING_PERIOD]: 'assets/voting.png',
+  [ProposalStatus.PROPOSAL_STATUS_PASSED]: 'assets/passed.png',
+  [ProposalStatus.PROPOSAL_STATUS_REJECTED]: 'assets/rejected.png',
+  [ProposalStatus.PROPOSAL_STATUS_FAILED]: 'assets/rejected.png'
+};
 
 @Component({
   standalone: true,
@@ -11,4 +25,6 @@ import { IProposal } from '../../../../models/paginated-proposals.model';
 })
 export class ProposalCardComponent {
   public readonly proposal = input.required<IProposal>();
+
+  protected readonly statusIcon = STATUS_ICON;
 }
--- src/app/models/paginated-proposals.model.ts
+++ src/app/models/paginated-proposals.model.ts
@@ -40,11 +40,29 @@ export interface IProposal {
 }
 
 export enum ProposalStatus {
+  /**
+   * Defines the default propopsal status
+   */
   PROPOSAL_STATUS_UNSPECIFIED = 'PROPOSAL_STATUS_UNSPECIFIED',
+  /**
+   * Defines a proposal status during the deposit period
+   */
   PROPOSAL_STATUS_DEPOSIT_PERIOD = 'PROPOSAL_STATUS_DEPOSIT_PERIOD',
+  /**
+   * Defines a proposal status during the voting period
+   */
   PROPOSAL_STATUS_VOTING_PERIOD = 'PROPOSAL_STATUS_VOTING_PERIOD',
+  /**
+   * Defines a proposal status of a proposal that has passed
+   */
   PROPOSAL_STATUS_PASSED = 'PROPOSAL_STATUS_PASSED',
+  /**
+   * Defines a proposal status of a proposal that has been rejected
+   */
   PROPOSAL_STATUS_REJECTED = 'PROPOSAL_STATUS_REJECTED',
+  /**
+   * Defines a proposal status of a proposal that has failed
+   */
   PROPOSAL_STATUS_FAILED = 'PROPOSAL_STATUS_FAILED'
 }
 
