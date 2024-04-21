7f240680aab99176094bb71f2a032a663ee3a5fc
AlexanderFSP <alexanderfsp@mail.ru>
2024-04-21 12:37:29 -0300

    ➕ Install and configure @ngneat/svg-icon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,13 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
+import { provideSvgIcons } from '@ngneat/svg-icon';
+
+import { cancelIcon } from './svg/cancel';
+import { checkmarkIcon } from './svg/checkmark';
+import { editIcon } from './svg/edit';
+import { plusIcon } from './svg/plus';
+import { unavailableIcon } from './svg/unavailable';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient()]
+  providers: [provideHttpClient(), provideSvgIcons([cancelIcon, checkmarkIcon, editIcon, plusIcon, unavailableIcon])]
 };
