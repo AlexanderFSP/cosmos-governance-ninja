ad98f2eb0bf8be3fb33943b84b332196e27d5580
AlexanderFSP <alexanderfsp@mail.ru>
2024-04-25 18:24:15 -0300

    🎨 Configure stylelint & execute it with fix flag + fix some errors manually

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-hudochenkov/order', 'stylelint-config-prettier-scss'],
+  ignoreFiles: ['!**/*'],
+  rules: {
+    'color-function-notation': null
+  }
+};
